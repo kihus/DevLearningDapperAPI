446883752b2823ed8cb4cdddd50740565dd3118b
juhfrancic <juliafrancictostes@gmail.com>
2025-11-29 01:21:40 -0300

    CRUD de Student

--- DevLearningAPI/DevLearningAPI/Models/StudentCourse.cs
+++ DevLearningAPI/DevLearningAPI/Models/StudentCourse.cs
@@ -2,4 +2,11 @@
 
 public class StudentCourse
 {
-}
+    public Guid CourseId { get; set; }
+    public Guid StudantId { get; set; }
+    public byte Progress { get; set; }
+    public bool Favorite { get; set; } = false;
+    public DateTime? StartUpdateDate { get; set; }
+
+    public StudentCourse() { } 
+    }
