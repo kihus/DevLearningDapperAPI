5272a8558cc4c89b4460cea4eb9cee83ca29e1db
GustavoRF1 <gustavo13rocha@gmail.com>
2025-11-29 15:40:09 -0300

    Add: Início de Author

--- DevLearningAPI/DevLearningAPI/Models/Author.cs
+++ DevLearningAPI/DevLearningAPI/Models/Author.cs
@@ -1,5 +1,13 @@
 ﻿namespace DevLearningAPI.Models;
 
-public class Author
+public class Author(string name, string title, string image, string bio, string url, string email, byte type)
 {
+    public Guid Id { get; private set; } = Guid.NewGuid();
+    public string Name { get; private set; } = name;
+    public string Title { get; private set; } = title;
+    public string Image { get; private set; } = image;
+    public string Bio { get; private set; } = bio;
+    public string Url { get; private set; } = url;
+    public string Email { get; private set; } = email;
+    public byte Type { get; private set; } = type;
 }
