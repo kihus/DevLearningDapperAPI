f87f3925cfa9bf790c86cf5a125b1b795500202f
GustavoRF1 <gustavo13rocha@gmail.com>
2025-11-29 00:24:20 -0300

    Adicionando CRUD de Categories

--- DevLearningAPI/DevLearningAPI/Models/Dtos/Category/CategoryResponseDto.cs
+++ DevLearningAPI/DevLearningAPI/Models/Dtos/Category/CategoryResponseDto.cs
@@ -2,4 +2,11 @@
 
 public class CategoryResponseDto
 {
+    public Guid Id { get; init; } = Guid.Empty;
+    public string Title { get; init; } = string.Empty;
+    public string Url { get; init; }= string.Empty;
+    public string Summary { get; init; } = string.Empty;
+    public int Order { get; init; } = 0;
+    public string Description { get; init; } = string.Empty;
+    public bool Featured { get; init; } = false;
 }
