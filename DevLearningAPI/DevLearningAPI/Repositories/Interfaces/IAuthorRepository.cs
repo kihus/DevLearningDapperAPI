8f448b5dcac8046a36e1a876c709155f2ad6ed3a
GustavoRF1 <gustavo13rocha@gmail.com>
2025-11-30 21:17:47 -0300

    Add: Author Intefaces

--- DevLearningAPI/DevLearningAPI/Repositories/Interfaces/IAuthorRepository.cs
+++ DevLearningAPI/DevLearningAPI/Repositories/Interfaces/IAuthorRepository.cs
@@ -1,5 +1,13 @@
-﻿namespace DevLearningAPI.Repositories.Interfaces;
+﻿using DevLearningAPI.Models;
+using DevLearningAPI.Models.Dtos.Author;
+
+namespace DevLearningAPI.Repositories.Interfaces;
 
 public interface IAuthorRepository
 {
+    Task<List<AuthorResponseDto>> GetAllAuthorsAsync();
+    Task<AuthorResponseDto> GetAuthorByIdAsync(Guid id);
+    Task CreateAuthorAsync(Author author);
+    Task UpdateAuthorAsync(Guid id, Author author);
+    Task UpdateTypeAuthorAsync(Guid id);
 }
--- DevLearningAPI/DevLearningAPI/Services/Interfaces/IAuthorService.cs
+++ DevLearningAPI/DevLearningAPI/Services/Interfaces/IAuthorService.cs
@@ -1,5 +1,12 @@
-﻿namespace DevLearningAPI.Services.Interfaces;
+﻿using DevLearningAPI.Models.Dtos.Author;
+
+namespace DevLearningAPI.Services.Interfaces;
 
 public interface IAuthorService
 {
+    Task<List<AuthorResponseDto>> GetAllAuthorsAsync();
+    Task<AuthorResponseDto> GetAuthorByIdAsync(Guid id);
+    Task CreateAuthorAsync(CreateAuthorDto author);
+    Task UpdateAuthorAsync(Guid id, UpdateAuthorDto authorRequest);
+    Task UpdateTypeAuthorAsync(Guid id);
 }
