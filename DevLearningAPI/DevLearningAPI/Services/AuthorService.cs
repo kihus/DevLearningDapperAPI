f018df4524edd46c9ee83297aad7c8444eaf4531
GustavoRF1 <gustavo13rocha@gmail.com>
2025-12-01 10:55:53 -0300

    refactor: OderBy in `Authors`

--- DevLearningAPI/DevLearningAPI/Services/AuthorService.cs
+++ DevLearningAPI/DevLearningAPI/Services/AuthorService.cs
@@ -7,72 +7,73 @@ namespace DevLearningAPI.Services;
 
 public class AuthorService : IAuthorService
 {
-	private readonly AuthorRepository _repository;
+    private readonly AuthorRepository _repository;
 
-	public AuthorService(AuthorRepository repository)
-	{
-		_repository = repository;
-	}
+    public AuthorService(AuthorRepository repository)
+    {
+        _repository = repository;
+    }
 
-	public async Task<List<AuthorResponseDto>> GetAllActiveAuthorsAsync()
-	{
-		return await _repository.GetAllActiveAuthorsAsync();
-	}
+    public async Task<List<AuthorResponseDto>> GetAllActiveAuthorsAsync()
+    {
+        return await _repository.GetAllActiveAuthorsAsync();
+    }
 
     public async Task<List<AuthorResponseDto>> GetAllAuthorsAsync()
     {
-        return await _repository.GetAllAuthorsAsync();
+        var authors = await _repository.GetAllAuthorsAsync();
+        return authors.OrderBy(x => x.Name).ToList();
     }
 
     public async Task<AuthorResponseDto> GetAuthorByIdAsync(Guid id)
-	{
-		return await _repository.GetAuthorByIdAsync(id);
-	}
+    {
+        return await _repository.GetAuthorByIdAsync(id);
+    }
 
-	public async Task CreateAuthorAsync(CreateAuthorDto author)
-	{
-		var newAuthor = new Author(
-			author.Name,
-			author.Title,
-			author.Image,
-			author.Bio,
-			author.Url,
-			author.Email
-			);
+    public async Task CreateAuthorAsync(CreateAuthorDto author)
+    {
+        var newAuthor = new Author(
+            author.Name,
+            author.Title,
+            author.Image,
+            author.Bio,
+            author.Url,
+            author.Email
+            );
 
-		await _repository.CreateAuthorAsync(newAuthor);
-	}
+        await _repository.CreateAuthorAsync(newAuthor);
+    }
 
-	public async Task UpdateAuthorAsync(Guid id, UpdateAuthorDto authorRequest)
-	{
-		var author = await _repository.GetAuthorByIdAsync(id);
+    public async Task UpdateAuthorAsync(Guid id, UpdateAuthorDto authorRequest)
+    {
+        var author = await _repository.GetAuthorByIdAsync(id);
 
-		var newAuthor = new Author(
-			string.IsNullOrEmpty(authorRequest.Name)
-								? author.Name
-								: authorRequest.Name,
-			string.IsNullOrEmpty(authorRequest.Title)
-								? author.Title
-								: authorRequest.Title,
-			string.IsNullOrEmpty(authorRequest.Image)
-								? author.Image
-								: authorRequest.Image,
-			string.IsNullOrEmpty(authorRequest.Bio)
-								? author.Bio
-								: authorRequest.Bio,
-			string.IsNullOrEmpty(authorRequest.Url)
-								? author.Url
-								: authorRequest.Url,
-			string.IsNullOrEmpty(authorRequest.Email)
-								? author.Email
-								: authorRequest.Email
-			);
+        var newAuthor = new Author(
+            string.IsNullOrEmpty(authorRequest.Name)
+                                ? author.Name
+                                : authorRequest.Name,
+            string.IsNullOrEmpty(authorRequest.Title)
+                                ? author.Title
+                                : authorRequest.Title,
+            string.IsNullOrEmpty(authorRequest.Image)
+                                ? author.Image
+                                : authorRequest.Image,
+            string.IsNullOrEmpty(authorRequest.Bio)
+                                ? author.Bio
+                                : authorRequest.Bio,
+            string.IsNullOrEmpty(authorRequest.Url)
+                                ? author.Url
+                                : authorRequest.Url,
+            string.IsNullOrEmpty(authorRequest.Email)
+                                ? author.Email
+                                : authorRequest.Email
+            );
 
-		await _repository.UpdateAuthorAsync(id, newAuthor);
-	}
+        await _repository.UpdateAuthorAsync(id, newAuthor);
+    }
 
-	public async Task UpdateTypeAuthorAsync(Guid id)
-	{
-		await _repository.UpdateTypeAuthorAsync(id);
-	}
+    public async Task UpdateTypeAuthorAsync(Guid id)
+    {
+        await _repository.UpdateTypeAuthorAsync(id);
+    }
 }
