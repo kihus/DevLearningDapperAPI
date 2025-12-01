e23acb815420a2104fa2717c0a26dfb4c3c900bc
Bruno <56200987+kihus@users.noreply.github.com>
2025-11-30 21:10:36 -0300

    Updates (#21)
    * corrigindo erros de build -> model CareerItem
    
    * trazendo mudanças de student
    
    ---------
    
    Co-authored-by: pedro-5by5 <pedro.gbelarmino@gmail.com>

---
+++ DevLearningAPI/DevLearningAPI/Controllers/StudentCourseController.cs
@@ -0,0 +1,101 @@
+﻿using DevLearningAPI.Models.Dtos.StudantCourse;
+using DevLearningAPI.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevLearningAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentCourseController : ControllerBase
+    {
+        private readonly StudentCourseService _service;
+
+        public StudentCourseController(StudentCourseService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<StudentCourseResponseDto>>> GetAllStudentCoursesAsync()
+        {
+            try
+            {
+                var studentCourses = await _service.GetAllStudentCoursesAsync();
+
+                if (studentCourses is null || !studentCourses.Any())
+                {
+                    return NotFound("No student-course relationships found.");
+                }
+
+                return Ok(studentCourses);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpPost("students/{studentId:guid}/courses/{courseId:guid}")]
+        public async Task<ActionResult> CreateStudentCourseAsync(Guid studentId, Guid courseId)
+        {
+            try
+            {
+
+                await _service.CreateStudentCourseAsync(studentId, courseId);
+                return Created("Student enrolled with sucess!", null);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpPut("update-progress/students/{studentId:guid}/courses/{courseId:guid}/progress/{minutesWatched:int}")]
+        public async Task<ActionResult> UpdateCourseProgressAsync(Guid studentId, Guid courseId, int minutesWatched)
+        {
+            try
+            {
+                var updatedProgress = await _service.UpdateCourseProgressAsync(studentId,
+                                                    courseId, minutesWatched);
+                if (updatedProgress == null)
+                    return NotFound(new { message = "Student-Course relationship not found." });
+
+                return Ok(new
+                {
+                    message = "Course progress updated successfully!",
+                    progress = updatedProgress
+                });
+
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "An error occurred while updating the course progress.");
+            }
+
+        }
+
+        [HttpPut("update-favorite/students/{studentId:guid}/courses/{courseId:guid}/favorite")]
+        public async Task<ActionResult> UpdateFavoriteStudentCourse(Guid studentId, Guid courseId)
+        {
+            try
+            {
+                var newValue = await _service.UpdateFavoriteStudentCourse(studentId, courseId);
+                if (newValue == null)
+                    return NotFound(new { message = "Student-Course relationship not found." });
+
+                return Ok(new
+                {
+                    message = "Favorite status updated successfully!",
+                    favorite = newValue
+                });
+
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "An error occurred while updating the favorite status.");
+            }
+
+        }
+    }
+}
